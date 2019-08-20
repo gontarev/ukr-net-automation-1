d577148c47f876805e4e1b02692db3d0b54b15ac
y.pernerovskyy <y.pernerovskyy@globallogic.com>
2019-08-20 19:33:01 +0300

    Class Added

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {    
+
+   public static void main(String[] args)
+   {
+     Integer a = 3;
+     Integer b = 4; 
+     
+     System.out.println(a+b);
+     System.out.println("Hello");
+   }   
+                                     
+}
