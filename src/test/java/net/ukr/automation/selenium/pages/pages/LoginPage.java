72753e418ad14a1cf17362462afc4b05365e4e2d
ysparrow <ysparrow@gmail.com>
2019-10-17 16:43:46 +0300

    Transform to PageFactory for LoginPage

--- src/test/java/net/ukr/automation/selenium/pages/pages/LoginPage.java
+++ src/test/java/net/ukr/automation/selenium/pages/pages/LoginPage.java
@@ -1,17 +1,32 @@
 package net.ukr.automation.selenium.pages.pages;
 
-
 import net.ukr.automation.selenium.pages.data.User;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 
 public class LoginPage extends Page {
 
+    @FindBy(id = "id-l")
+    private WebElement login;
+
+    @FindBy(id = "id-p")
+    private WebElement password;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement submit;
+
+    @FindBy(css = "button.default.compose")
+    private WebElement composeBtn;
+
     public LoginPage(WebDriver driver)
     {
         super(driver);
+        PageFactory.initElements(driver, this);
+
     }
 
     public void open()
@@ -22,9 +37,9 @@ public class LoginPage extends Page {
     public void login(User user)
     {
         open();
-        driver.findElement(By.id("id-l")).sendKeys(user.getLogin());
-        driver.findElement(By.id("id-p")).sendKeys(user.getPassword());
-        driver.findElement(By.cssSelector("button[type='submit']")).click();
-        wait.until(elementToBeClickable(By.cssSelector("button.default.compose")));
+        login.sendKeys(user.getLogin());
+        password.sendKeys(user.getPassword());
+        submit.click();
+        wait.until(elementToBeClickable(composeBtn));
     }
 }
